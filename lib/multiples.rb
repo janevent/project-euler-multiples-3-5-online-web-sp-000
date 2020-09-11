60115ed3eb3822be608ee1588883af99c40e06cc
Jane Venter <janeanneventer@gmail.com>
2020-09-11 20:12:04 +0000

    Automatically backed up by Learn

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -7,7 +7,7 @@ def collect_multiples(limit)
       times5 = 5*i
       if times3 < limit
         array.push(times3)
-        put times3
+        puts times3
       end
       if times5 < limit
         array.push(times5)
