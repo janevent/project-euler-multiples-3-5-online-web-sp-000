9e34860a873e464dd9ef3b6228dd07670d5595ee
Jane Venter <janeanneventer@gmail.com>
2020-09-11 20:04:20 +0000

    Automatically backed up by Learn

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+   array = []
+   i = 0
+   until i == limit do
+      times3 = 3*i 
+      times5 = 5*i
+      if times3 < limit
+        array.push(times3)
+      end
+     array.push(5*i)
+     i+=1
+     
+
+     
+   end
+   #array.reduce(0){ |i, t| i + t }
+   array
+end
