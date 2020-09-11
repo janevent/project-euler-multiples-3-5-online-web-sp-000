053a6a202f9e3bcb5682669815989fc03c9ac888
Jane Venter <janeanneventer@gmail.com>
2020-09-11 20:23:34 +0000

    Automatically backed up by Learn

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,7 +1,15 @@
 # Enter your object-oriented solution here!
 class Multiples
-  
+  attr_reader: @limit
   def initialize(limit)
     @limit = limit
   end
+  
+  def collect_multiples
+    
+  end
+  
+  def sum_multiples
+  end
+  
 end
